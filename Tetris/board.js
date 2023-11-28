d10fd02d09dd5be6fed59f89ea41c29ccf46e7d2
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2023-11-28 10:45:33 -0500

    Add files via upload

---
+++ Tetris/board.js
@@ -0,0 +1,13 @@
+//board.js
+class Board { 
+    constructor(ctx) {
+      this.ctx = ctx;
+      this.grid = this.getEmptyBoard();
+    }
+   
+    getEmptyBoard() {
+      return Array.from(
+        {length: ROWS}, () => Array(COLS).fill(0)
+      );
+    }
+  }
